9ee6d3652c1f345b3029c8925d5edcc4e250386d
Heidi <heidi.climber@gmail.com>
2026-02-20 10:50:27 -0700

    toml started, SE started

--- src/bcgeDataPy/SEPy.py
+++ src/bcgeDataPy/SEPy.py
@@ -8,3 +8,7 @@ class SummarizedExperimentPy():
     def assay(self):
         exp_data = pd.read_csv(self.data, sep="\t")
         return exp_data  
+    
+    def colData(self):
+        meta = pd.read_csv(self.metadata, sep="\t")
+        return meta 
