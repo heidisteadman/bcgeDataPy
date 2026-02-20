98b2f5d01e16a1b9bd1826befd2ecf7c15cab9f8
Heidi <heidi.climber@gmail.com>
2026-02-20 10:24:40 -0700

    setup, started on SE class

---
+++ src/bcgeDataPy/SEPy.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+class SummarizedExperimentPy():
+    def __init__(self, data, metadata):
+        self.data = data 
+        self.metadata = metadata 
+    
+    def assay(self):
+        exp_data = pd.read_csv(self.data, sep="\t")
+        return exp_data  
