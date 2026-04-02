15bd23b1736d548b971d3736a305f0db5726771a
Heidi <heidi.climber@gmail.com>
2026-04-02 11:12:40 -0600

    fixed searchForDatasetFunctions

--- src/bcgeDataPy/helpers.py
+++ src/bcgeDataPy/helpers.py
@@ -23,21 +23,3 @@ def chooseVersion(datasetID: str, dataset: ZenObj, cache: BiocFileCache|str, v: 
     metadata = (Path(f"{dirPath}/{datasetID}_metadata.tsv"))
 
     return SummarizedExperimentPy(expData, metadata)
-
-def downloadZenFile(conceptDOI:str, path:Path) -> None:
-    cdoi = f"conceptdoi:{conceptDOI}"
-    jsonresp = requests.get(url="https://zenodo.org/api/records",
-                                     params={
-                                         "q": cdoi,
-                                         "size": 25,
-                                         "sort": "mostrecent"
-                                     })
-
-    js = jsonresp.json()
-    hits = js['hits']['hits'][0]
-    doi_id = hits['id']
-    download_link = f"https://zenodo.org/api/records/, {doi_id}, /files-archive"
-    downloaded_file = requests.get(download_link)
-    with open(path, 'wb') as f:
-        f.write(downloaded_file.content)
-    return
--- src/bcgeDataPy/searchForDatasets.py
+++ src/bcgeDataPy/searchForDatasets.py
@@ -1,16 +1,18 @@
 import pandas as pd
-from helpers import downloadZenFile 
 import tempfile 
 from pathlib import Path
+from ZenodoObj import ZenObj
 
 def searchForDatasetFields(terms: list[str]) -> pd.DataFrame:
     identifiers = ['10.5281/zenodo.17583904', 'filtered_mapped_data.tsv.gz']
-    dataPath = Path(f"{tempfile.gettempdir()}/{identifiers[1]}")
+    dataPath = Path(f"{tempfile.gettempdir()}/mappedData")
+    filePath = dataPath / identifiers[1]
+    zendata = ZenObj(identifiers[0])
+    if not filePath.exists():
+        download_link = zendata.parse_json()
+        zendata.download_file(download_link, dataPath)
     
-    if not dataPath.exists():
-        downloadZenFile(identifiers[0], dataPath)
-    
-    mappedTerms = pd.read_csv(dataPath, sep= "\t")
+    mappedTerms = pd.read_csv(filePath, sep= "\t")
     matches = mappedTerms[mappedTerms['NCIT_field_code'].isin(terms)]
     matches = matches[['dataset', 'orig_field', 'NCIT_field_code']]
     matches = matches.rename(columns={'dataset': 'Dataset_ID', 'orig_field': 'Field', 'NCIT_field_code': 'Code'})
@@ -19,12 +21,14 @@ def searchForDatasetFields(terms: list[str]) -> pd.DataFrame:
 
 def searchForDatasetValues(terms: list[str]) -> pd.DataFrame:
     identifiers = ['10.5281/zenodo.17583904', 'filtered_mapped_data.tsv.gz']
-    dataPath = Path(f"{tempfile.gettempdir()}/{identifiers[1]}")
+    dataPath = Path(f"{tempfile.gettempdir()}/mappedData")
+    filePath = dataPath / identifiers[1]
+    zendata = ZenObj(identifiers[0])
+    if not filePath.exists():
+        download_link = zendata.parse_json()
+        zendata.download_file(download_link, dataPath)
     
-    if not dataPath.exists():
-        downloadZenFile(identifiers[0], dataPath)
-    
-    mappedTerms = pd.read_csv(dataPath, sep= "\t")
+    mappedTerms = pd.read_csv(filePath, sep= "\t")
     matches = mappedTerms[mappedTerms['NCIT_value_code'].isin(terms)]
     matches = matches[['dataset', 'orig_field', 'NCIT_field_code', 'orig_values', 'NCIT_value_code']]
     matches = matches.rename(columns={'dataset': 'Dataset_ID', 'orig_field': 'Field', 'NCIT_field_code': 'Field_Code', 'orig_values': 'Values', 'NCIT_value_code': 'Code'})
